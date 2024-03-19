f7168db64e26c61dd67142cc1c3d81210d35c118
jeonminhun <alsgns99@naver.com>
2024-03-19 16:29:18 +0900

    원상복구

--- src/main/java/com/spring/myapp/member/LoginForm.java
+++ src/main/java/com/spring/myapp/member/LoginForm.java
@@ -7,10 +7,9 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 @Component("loginForm")
-// 아무거나 작성함 222222
 @Data
 public class LoginForm {
 	private String useraccount;
 	private String password;
-////
+
 }
